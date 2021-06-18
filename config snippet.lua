e8e5ff5253f233fa126156cc02409846c57ba472
timginter <gaertner.t@gmail.com>
2021-06-18 14:40:06 +0100

    trigger toggle on key release instead of press, tidy up code

--- config snippet.lua
+++ config snippet.lua
@@ -12,6 +12,7 @@ local suppressMouseForward = true
 local muteKeyPressed = false
 
 function toggleMsTeamsMute()
+    -- TODO: send keystrokes to call window, not to application (last active window of the app)
     local msTeams = hs.appfinder.appFromName("Microsoft Teams")
     if msTeams ~= nil then
         hs.eventtap.event.newKeyEvent(hs.keycodes.map.shift, true):post(msTeams)
@@ -21,49 +22,47 @@ function toggleMsTeamsMute()
         hs.eventtap.event.newKeyEvent(hs.keycodes.map.shift, false):post(msTeams)
         hs.eventtap.event.newKeyEvent(hs.keycodes.map.cmd, false):post(msTeams)
     end
-
-    -- update pressed status
-    muteKeyPressed = not muteKeyPressed
 end
 
 -- capture mouse button "Forward"
 overrideMouseForwardDown = hs.eventtap.new({ hs.eventtap.event.types.otherMouseDown }, function(e)
+    -- mouse button "Forward"
     if e:getProperty(hs.eventtap.event.properties['mouseEventButtonNumber']) == 4 then
-        toggleMsTeamsMute()
+        -- toggle mute only if pushToTalk is enabled
+        if pushToTalk then
+            toggleMsTeamsMute()
+        end
+
+        -- update pressed status
+        muteKeyPressed = true
 
         return suppressMouseForward
     end
 end)
 overrideMouseForwardUp = hs.eventtap.new({ hs.eventtap.event.types.otherMouseUp }, function(e)
-    if e:getProperty(hs.eventtap.event.properties['mouseEventButtonNumber']) == 4 then-- suppress keyUp event if pushToTalk disabled
-        -- suppress keyUp event if pushToTalk disabled
-        if not pushToTalk and muteKeyPressed then
-            -- update pressed status
-            muteKeyPressed = not muteKeyPressed
-
-            return suppressMouseForward
-        end
-
+    -- mouse button "Forward"
+    if e:getProperty(hs.eventtap.event.properties['mouseEventButtonNumber']) == 4 then
         toggleMsTeamsMute()
+
+        -- update pressed status
+        muteKeyPressed = false
 
         return suppressMouseForward
     end
 end)
 
 -- capture Fn key
-overrideFnForTeamsMute = hs.eventtap.new({ hs.eventtap.event.types.flagsChanged }, function(e)
-    -- Fn key Down or Up
+overrideFn = hs.eventtap.new({ hs.eventtap.event.types.flagsChanged }, function(e)
+    -- only Fn key, KeyDown or KeyUp events
     if e:getType() == 12 and e:getKeyCode() == 63 then
-        -- suppress keyUp event if pushToTalk disabled
-        if not pushToTalk and muteKeyPressed then
-            -- update pressed status
-            muteKeyPressed = not muteKeyPressed
-
-            return suppressFnKey
+        -- toggle mute only if pushToTalk or KeyUp event
+        if pushToTalk or muteKeyPressed then
+            toggleMsTeamsMute()
         end
 
-        toggleMsTeamsMute()
-        
+        -- update pressed status
+        muteKeyPressed = not muteKeyPressed
+
         return suppressFnKey
     end
 end)
@@ -71,7 +70,7 @@ end)
 -- start override functions
 overrideMouseForwardDown:start()
 overrideMouseForwardUp:start()
-overrideFnForTeamsMute:start()
+overrideFn:start()
 
 ------------------------------------------------------------------------------------------
 -- END OF MS TEAMS Push-To-Talk
